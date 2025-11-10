cd82cdeaf90feede21dfb8eec80b58cbafccb073
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-11-10 17:22:11 +0600

    create collection mongodb and data pass client side to server side

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 const express = require('express')
 const cors = require('cors')
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = "mongodb+srv://Car-rent-DB:YSZF06ecNMLfPUf5@cluster0.xhsdu7s.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db=client.db("car_rent_db")
+    const carCollection=db.collection("all_cars")
+
+    app.get('/all_cars', async(req, res)=>{
+      const result=await carCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/all_cars', async(req, res)=>{
+      const newCar=req.body
+      console.log(newCar);
+      const result=await carCollection.insertOne(newCar)
+      res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Car rental server is running')
